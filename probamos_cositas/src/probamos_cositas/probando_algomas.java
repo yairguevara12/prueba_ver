e8b0cf4a4a016e7cadd2fef9eb6a8a6dcff5bf6d
mlezama20212021 <lezama2021@yahoo.com>
2022-05-04 17:46:51 -0500

    estoy subiendo algo mas

---
+++ probamos_cositas/src/probamos_cositas/probando_algomas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probamos_cositas;
+
+/**
+ *
+ * @author yair
+ */
+public class probando_algomas {
+    String ga = "a";
+}
